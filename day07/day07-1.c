dc602d81317e192807f6fd9cb5a0507e24ead9cd
Songkangmin24 <erhar0107@gmail.com>
2024-10-10 14:36:04 +0900

    Update day07-1.c

--- day07/day07-1.c
+++ day07/day07-1.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int num = 5;
+	int num = 5;//피라미드의 줄수 지정
 	int i, j;
 
 	for (i = 0; i < num; i++) {
-		for (j = num-i-1; j >0 ;j--) {
-			printf(" ");
+		for (j = num-i-1; j >0 ;j--) { //피라미드 왼쪽 공백 출력
+			printf(" "); 
 		}
-		for (j = 1; j <= (i*2)+1; j++) {
+		for (j = 1; j <= (i*2)+1; j++) { //피라미드 * 출력
 			printf("*");
 		}
 		printf("\n");
